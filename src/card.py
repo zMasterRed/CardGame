adb9b9a8acd826bcadc58deeade19554358cd8fb
zMasterRed <matteosommario@gmail.com>
2026-03-08 11:51:30 +0100

    fix: fixed space problems with black and other lint error

--- src/card.py
+++ src/card.py
@@ -1,14 +1,14 @@
 class Card:
-    def __init__(self, value: int , is_joker: bool = False):
-     
+    def __init__(self, value: int, is_joker: bool = False):
+
         self.value = value
         self.is_joker = is_joker
 
     def __eq__(self, other):
-    
+
         if not isinstance(other, Card):
             return False
-        
+
         return self.value == other.value
 
     def debug(self):
