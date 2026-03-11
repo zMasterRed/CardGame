4bd5d1d545c6b645bc21e157a7f426054de2d3b9
Riccardo <109389972+Ricky405@users.noreply.github.com>
2026-03-11 17:16:15 +0100

    fix: minor error

--- src/table.py
+++ src/table.py
@@ -13,14 +13,13 @@ class TableView(arcade.View):
         self.txt_enemy_c = None
         self.txt_player_c = None
 
-
         self.path_joker = ":resources:images/cards/cardJoker.png"
         self.path_card_back = ":resources:images/cards/cardBack_blue2.png"
 
         self.card_X = 56
         self.card_Y = 76
-        
-        self. setup()
+
+        self.setup()
 
     def setup(self):
 
@@ -33,44 +32,34 @@ class TableView(arcade.View):
             p_heart = arcade.Text("♥", pos_heart + (i * 25), 305, (166, 23, 13), 20)
             self.player_heart.append(p_heart)
 
-        card = ["A","2","3","4","5","6","7","8","9"]
-
         self.txt_enemy_c = arcade.Text(
-            "Enemy couples: ", 30, 375,
-            arcade.color.WHITE, 14, bold = True
+            "Enemy couples: ", 30, 375, arcade.color.WHITE, 14, bold=True
         )
         self.txt_player_c = arcade.Text(
-            "Player couples: ", 30, 315,
-            arcade.color.WHITE, 14, bold = True
+            "Player couples: ", 30, 315, arcade.color.WHITE, 14, bold=True
         )
-
 
     def on_show_view(self):
         arcade.set_background_color((5, 105, 25))
 
     def on_draw(self):
-        self.clear();
+        self.clear()
 
-        arcade.draw_line(0, settings.fY/2, settings.fX, settings.fY/2,arcade.color.WHITE, 1)
+        arcade.draw_line(
+            0, settings.fY / 2, settings.fX, settings.fY / 2, arcade.color.WHITE, 1
+        )
 
         for i in self.player_heart:
             i.draw()
         for i in self.enemy_heart:
             i.draw()
 
-        
         self.txt_enemy_c.draw()
         self.txt_player_c.draw()
 
-
-
         arcade.draw_rect_outline(
-            arcade.XYWH(85, 460, 75,105),
-            arcade.color.WHITE,
-            border_width = 2
+            arcade.XYWH(85, 460, 75, 105), arcade.color.WHITE, border_width=2
         )
         arcade.draw_rect_outline(
-            arcade.XYWH(85, 240, 75,105),
-            arcade.color.WHITE,
-            border_width = 2
+            arcade.XYWH(85, 240, 75, 105), arcade.color.WHITE, border_width=2
         )
