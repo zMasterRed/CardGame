34734baaaf41e1a573eac2c9032fa704b3df4327
Riccardo <109389972+Ricky405@users.noreply.github.com>
2026-03-11 11:44:06 +0100

    Implemented def and drew hearts

--- src/table.py
+++ src/table.py
@@ -21,4 +21,13 @@ class TableView(arcade.View):
             p_heart = arcade.Text("♥", pos_heart + (i * 25), 315, (166, 23, 13), 20)
             self.player_heart.append(p_heart)
 
-        
+    def on_show_view(self):
+        arcade.set_background_color((5, 105, 25))
+    
+    def on_draw(self):
+        self.clear();
+
+        for i in self.player_heart:
+            i.draw()
+        for i in self.enemy_heart:
+            i.draw()
