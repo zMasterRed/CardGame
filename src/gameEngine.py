b683f9a08b883e406c89cb0e9df76a12a93e8207
zMasterRed <matteosommario@gmail.com>
2026-03-13 18:00:30 +0100

    feat: add enemy_draws_card function

--- src/gameEngine.py
+++ src/gameEngine.py
@@ -69,9 +69,6 @@ class GameEngine:
             self.enemy.hand.remove(card)
             self.player.hand.append(card)
 
-            if card.is_joker:
-                self.apply_damage(is_player=True)
-
             self.player.check_pairs()
             self.update_game_status()
 
@@ -81,3 +78,23 @@ class GameEngine:
                 self.game_status = "WIN"
             elif len(self.enemy.hand) == 0:
                 self.game_status = "LOSE"
+
+    def enemy_draws_card(self) -> Card:
+            """
+            Enemy draws a random card from player hand,
+            return it to display in the updated deck
+            """
+            if not self.player or not self.enemy:
+                raise PlayerNotFound()
+
+            if len(self.player.hand) == 0:
+                return None
+
+            random_card = random.choice(self.player.hand)
+            self.player.hand.remove(random_card)
+            self.enemy.hand.append(random_card)
+
+            self.enemy.check_pairs()
+            self.update_game_status()
+
+            return random_card
