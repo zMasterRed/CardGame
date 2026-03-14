ef14061ad77ce07e7d9fe2b1e18287957600f4f3
zMasterRed <matteosommario@gmail.com>
2026-03-14 10:27:13 +0100

    refactor: improve player class type hints

--- src/player.py
+++ src/player.py
@@ -3,12 +3,12 @@ from src.exceptions import InsufficientHealth
 
 
 class Player:
-    def __init__(self, hand: list[Card], health: int = 3):
+    def __init__(self, hand: list[Card], health: int = 3) -> None:
         self.hand = hand
         self.health = health
 
-    def check_pairs(self):
-        new_hand = []
+    def check_pairs(self) -> None:
+        new_hand: list[Card] = []
         for card in self.hand:
             if card in new_hand:
                 new_hand.remove(card)
@@ -16,10 +16,10 @@ class Player:
                 new_hand.append(card)
         self.hand = new_hand
 
-    def decrease_health(self):
+    def decrease_health(self) -> None:
         if self.health <= 0:
             raise InsufficientHealth()
         self.health -= 1
 
-    def is_dead(self):
+    def is_dead(self) -> bool:
         return self.health <= 0
