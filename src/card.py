983c6fa9826d6b4f626bc4674627a886fe5ad5fe
zMasterRed <matteosommario@gmail.com>
2026-03-14 10:23:09 +0100

    refactor: improve card class type hints

--- src/card.py
+++ src/card.py
@@ -2,7 +2,7 @@ import arcade
 
 
 class Card(arcade.Sprite):
-    def __init__(self, value: int, is_joker: bool = False):
+    def __init__(self, value: int, is_joker: bool = False) -> None:
 
         self.value = value
         self.is_joker = is_joker
@@ -13,29 +13,29 @@ class Card(arcade.Sprite):
 
         super().__init__(self.path_front, scale=0.4)
 
-    def __eq__(self, other):
+    def __eq__(self, other : object) -> bool:
         if not isinstance(other, Card):
             return False
 
         return self.value == other.value
 
-    def get_path(self):
+    def get_path(self) -> str:
         if self.is_joker:
             return ":resources:images/cards/cardJoker.png"
         # C = is the Card Value
         C = "A" if self.value == 1 else str(self.value)
         return f":resources:images/cards/cardSpades{C}.png"
 
-    def flip(self, face_up: bool):
+    def flip(self, face_up: bool) -> None:
         self.face_up = face_up
         self.texture = arcade.load_texture(
             self.path_front if face_up else self.path_back
         )
 
-    def debug(self):
+    def debug(self) -> str:
         if self.is_joker:
             return "[ JOKER ]"
         return f"[{self.value}]"
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(id(self))
